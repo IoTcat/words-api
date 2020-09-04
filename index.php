d55ef9ca01293dabda7582936270fd4d2bc4d45b
cn.yimian.xyz <i@iotcat.me>
2020-09-04 16:23:59 +0800

    en

--- index.php
+++ index.php
@@ -11,12 +11,16 @@ include './functions.php';
 
 header('Access-Control-Allow-Origin:*');
 
+$lang = $_GET['lang'];
+if(!isset($lang)) $lang = 'zh';
+
 $conn = db__connect("yulu");
 
-$rand = rand(1, db__rowNum($conn, "yulu"));
+$words = db__getData($conn, "yulu", "lang", $lang);
 
-$words = db__getData($conn, "yulu", "wid", $rand);
+$rand = rand(0, count($words));
 
-echo $words[0]['words'];
+
+echo $words[$rand]['words'];
 
 yimian__log("log_api", array("api" => "words", "timestamp" => date('Y-m-d H:i:s', time()), "ip" => ip2long(getIp()), "_from" => get_from(), "content" => $words[0]['words']));
